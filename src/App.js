13869812ac6766636132f99221cca831c253e4a6
Rita Cunha <rita.cunha.ts@gmail.com>
2022-03-03 18:51:39 +0000

    missing axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import CountriesList from './components/CountriesList/CountriesList';
+import CountryDetails from './components/CountryDetails/CountryDetails';
+import { Routes, Route } from 'react-router-dom';
+
+import CountriesData from './countries.json'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <div className="container">
+        <div className="row">
+          <CountriesList countries={CountriesData} />
+          {
+            <Routes>
+              <Route
+                path="/:id"
+                element={<CountryDetails />}
+              />
+            </Routes>
+          }
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/CountryDetails/CountryDetails.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Link, useParams } from 'react-router-dom';
+import countriesData from '../../countries.json';
+
+function CountryDetails() {
+  const { id } = useParams();
+  const foundCountry = countriesData.find((oneCountry) => {
+    return oneCountry.alpha3Code === id;
+  });
+
+  const getCountryName = (alpha3Code) => {
+    const foundCountryName = countriesData.find((oneCountry) => {
+      return oneCountry.alpha3Code === alpha3Code;
+    });
+    return foundCountryName.name.common;
+  };
+
+  return (
+    <div key={foundCountry.alpha3Code} className="col-7">
+      <img
+        src={` https://flagpedia.net/data/flags/icon/72x54/${foundCountry.alpha2Code.toLowerCase()}.png`}
+        alt={foundCountry.name.official}
+      />
+      <h1>{foundCountry.name.official}</h1>
+      {!foundCountry && <h1>No Country Found!</h1>}
+      {foundCountry && (
+        <>
+          <table className="table">
+            <thead></thead>
+            <tbody>
+              <tr>
+                <td style={{ width: '30%' }}>Capital</td>
+                <td>{foundCountry.capital}</td>
+              </tr>
+              <tr>
+                <td>Area</td>
+                <td>
+                  {foundCountry.area} km
+                  <sup>2</sup>
+                </td>
+              </tr>
+              <tr>
+                <td>Borders</td>
+                <td>
+                  <ul style={{listStyle:"none"}}>
+                    {foundCountry.borders.map((country) => {
+                      return (
+                        <li>
+                          <Link to={`/${country}`}>{getCountryName(country)}</Link>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default CountryDetails;
